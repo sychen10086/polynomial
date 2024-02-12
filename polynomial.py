4cc946af06c356565545bb0cda1841bd665bf7c5
Sam <sam@SamdeMacBook-Air.local>
2024-02-12 00:34:27 -0500

    cs 506 exercise part c

--- polynomial.py
+++ polynomial.py
@@ -5,7 +5,8 @@ class X:
     def __repr__(self):
         return "X"
     
-
+    def evaluate(self, value):
+        return value
 
 class Int:
     def __init__(self, i):
@@ -14,7 +15,8 @@ class Int:
     def __repr__(self):
         return str(self.i)
 
-
+    def evaluate(self, value):
+        return self.i
 class Add:
     def __init__(self, p1, p2):
         self.p1 = p1
@@ -23,7 +25,8 @@ class Add:
     def __repr__(self):
         return repr(self.p1) + " + " + repr(self.p2)
     
-
+    def evaluate(self, value):
+        return self.p1.evaluate(value) + self.p2.evaluate(value)
 
 class Mul:
     def __init__(self, p1, p2):
@@ -45,7 +48,8 @@ class Mul:
             return repr(self.p1) + " * ( " + repr(self.p2) + " )"
         return repr(self.p1) + " * " + repr(self.p2)
     
-
+    def evaluate(self, value):
+        return self.p1.evaluate(value) * self.p2.evaluate(value)
     
 class Div:
     def __init__(self, p1, p2):
@@ -57,7 +61,12 @@ class Div:
         right = f"( {repr(self.p2)} )" if isinstance(self.p2, (Add, Sub, Mul)) else repr(self.p2)
         return f"{left} / {right}"
     
-
+    def evaluate(self, value):
+        # It's important to handle the case where p2 evaluates to zero.
+        p2_eval = self.p2.evaluate(value)
+        if p2_eval == 0:
+            raise ZeroDivisionError("Division by zero in polynomial evaluation.")
+        return self.p1.evaluate(value) / p2_eval
     
 class Sub:
     def __init__(self, p1, p2):
@@ -69,6 +78,11 @@ class Sub:
         right = repr(self.p2)
         return f"{left} - {right}"
     
+    def evaluate(self, value):
+        return self.p1.evaluate(value) - self.p2.evaluate(value)
+
 
 poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
 print(poly)
+poly = Add( Add( Int(4), Int(3)), Add( X(), Mul( Int(1), Add( Mul(X(), X()), Int(1)))))
+print(poly.evaluate(-1))
